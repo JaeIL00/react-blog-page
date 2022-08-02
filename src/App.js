162294f6af6e1c4afe5a9911084798f2f1cd8dba
ShinJaeIl01 <ekvm555@naver.com>
2022-08-02 15:12:26 +0900

    feat: Create Modal UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [contentTitle, setContentTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
+  const [thumbs, setThumbs] = useState(0)
+  let [modal, setModal] = useState(false)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <button onClick={() => {
+        let copy = [...contentTitle]
+        copy[0] = '여자 코트 추천'
+        setContentTitle(copy)
+        }}>
+        글 수정
+      </button>
+      <div className="list">
+        <h4 onClick={() => { setModal(!modal) }}>{contentTitle[0]} <span onClick={()  => {setThumbs(thumbs+1)}}>👍</span> {thumbs} </h4>
+        <p>7월 30일 발행</p>
+      </div>
+      <div className="list">
+      <h4>{contentTitle[1]}</h4>
+        <p>7월 30일 발행</p>
+      </div>
+      <div className="list">
+      <h4>{contentTitle[2]}</h4>
+        <p>7월 30일 발행</p>
+      </div>
+
+      {
+        modal === true ? <Modal /> : null
+      }
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
